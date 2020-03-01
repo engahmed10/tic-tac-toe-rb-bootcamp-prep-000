0bb69bc1b1c979c1a54f2be36bc77ba07c393a57
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-01 12:04:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip
+  input = gets.chomp
   index=input_to_index(input)
   if valid_move?(board,index)
     move(board,index,name="X")

4a81518d55a7719332af3bc2cee982d626b1f1c0
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-01 12:06:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,13 @@ end
 
 # code your #valid_move? method here
 def valid_move?(board, index)
-    number_entered.between?(0, 8) && !(position_taken?(board, index))
+  if position_taken?(board, index)
+      false
+    elsif index > board.length() || index < 0
+     false
+    else
+     true
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.

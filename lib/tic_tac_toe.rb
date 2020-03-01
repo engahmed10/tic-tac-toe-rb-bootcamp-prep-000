edc6b241855701ab872142e5f68086ebf419a9ac
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-01 12:09:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -130,7 +130,7 @@ def play(board)
         turn(board)
       end
       if won?(board)
-            puts "Congratulations #{won?(board)[0]}!"
+            puts "Congratulations #{winner(board)}!"
       elsif draw?(board)
             puts "Cat's Game!"
       end

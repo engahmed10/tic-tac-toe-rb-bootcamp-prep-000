d44b42aa0f6d3585bd92a0288f8be5db79166a7b
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-01 12:42:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,7 @@ def turn(board)
   input = gets.strip
   index=input_to_index(input)
   if valid_move?(board,index)
-    move(board,index,name="X")
+    move(board,index,name="O")
     display_board(board)
   else
    turn(board)
